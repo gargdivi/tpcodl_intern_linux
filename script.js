6298361e5a504b200edac34a38b79023186e3fca
Divit Garg <147988200+gargdivi@users.noreply.github.com>
2025-05-30 15:42:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple script to show a greeting in the console
+console.log("Welcome to TPCODL - DevOps Project Loaded");
+
+// Optional: show alert on page load
+window.onload = () => {
+  alert("Welcome to TPCODL!");
+};
